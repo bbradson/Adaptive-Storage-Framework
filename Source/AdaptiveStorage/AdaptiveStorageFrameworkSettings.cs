a42dcd810abf7d4da49be844baa34531c90fc1f8
bbradson <bradson@duck.com>
2025-05-19 14:55:59 +0200

    Fix settings throwing error when opened in main menu

--- Source/AdaptiveStorage/AdaptiveStorageFrameworkSettings.cs
+++ Source/AdaptiveStorage/AdaptiveStorageFrameworkSettings.cs
@@ -137,7 +137,9 @@ public class AdaptiveStorageFrameworkSettings : ModSettings
 			var cameraZoomRangeIndex = Array.IndexOf(_cameraZoomRangeSteps, _labelHidingMaxZoomLevel);
 			listing.CollectionSlider(_cameraZoomRangeSteps, ref cameraZoomRangeIndex,
 				Strings.Translated.ASF_MaxZoomLevelSetting, Strings.Translated.ASF_MaxZoomLevelDescription,
-				static zoomLevel => GetZoomLevelValue(zoomLevel).ToString());
+				static zoomLevel => Current.CameraDriver is var driver && driver != null && driver.config is { } config
+					? GetZoomLevelValue(zoomLevel, config.sizeRange).ToString()
+					: string.Empty);
 			_labelHidingMaxZoomLevel = _cameraZoomRangeSteps[cameraZoomRangeIndex];
 		}
 
